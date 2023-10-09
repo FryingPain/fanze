3a47e0a8a0f7973ffa0628e7001b6114ab2ffd82
FryingPain <100082354+FryingPain@users.noreply.github.com>
2023-10-09 20:36:02 -0300

    Update Waiting Room Redirect.user.js

--- userscripts/Waiting Room Redirect.user.js
+++ userscripts/Waiting Room Redirect.user.js
@@ -2,7 +2,7 @@
 // @name         Waiting Room Redirect
 // @namespace    https://www.fryingpain.com
 // @homepage     https://www.fryingpain.com/userscripts/
-// @version      1.0
+// @version      1.1
 // @description  Redirects from the waiting room to a 2b2t style waiting room.
 // @author       FryingPain (ChatGPT)
 // @match        https://beta.character.ai/*
@@ -16,7 +16,12 @@
     const waitTimeHeader = document.querySelector("h2");
     if (waitTimeHeader && waitTimeHeader.textContent.includes("Your estimated wait time is")) {
         // Extract the remaining minutes from the text
-        const remainingTime = extractRemainingTime(waitTimeHeader.textContent);
+        if (waitTimeHeader && waitTimeHeader.textContent.includes("hour")) {
+            var remainingTime = extractRemainingTime(waitTimeHeader.textContent);
+            remainingTime = remainingTime*60;
+        } else {
+          var remainingTime = extractRemainingTime2(waitTimeHeader.textContent);
+        }
 
         // Redirect to the personalized HTML page with the time parameter
         window.location.href = `https://www.fryingpain.com/waiting.html?time=${remainingTime}`;
@@ -24,7 +29,19 @@
 
     function extractRemainingTime(text) {
         // Extract the remaining time from the text using regular expressions
-        const regex = /Your estimated wait time is (\d+) minute/;
+        const regex = /Your estimated wait time is more than (\d+) hour[s]?.../;
+        const match = text.match(regex);
+
+        if (match && match[1]) {
+            return match[1];
+        }
+
+        // If no match is found, return a default value
+        return "unknown";
+    }
+    function extractRemainingTime2(text) {
+        // Extract the remaining time from the text using regular expressions
+        const regex = /Your estimated wait time is (\d+) minute[s]?.../;
         const match = text.match(regex);
 
         if (match && match[1]) {
