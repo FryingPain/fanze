a338513e8069028b9ab107fc6bc65b852b3e2b4d
FryingPain <100082354+FryingPain@users.noreply.github.com>
2024-07-05 09:06:57 -0400

    parappa update :3

--- userscripts/ParappaGoogleSearch.user.js
+++ userscripts/ParappaGoogleSearch.user.js
@@ -2,7 +2,7 @@
 // @name         Parappa Google Search
 // @namespace    https://www.fryingpain.com
 // @homepage     https://www.fryingpain.com/userscripts/
-// @version      1.1.0
+// @version      2.0.0
 // @description  Search images. now in parappa style!
 // @author       FryingPain & AI :3
 // @match        *://www.google.com/search*
@@ -13,13 +13,15 @@
 
 /*
 
+HUGE UPDATE: new GUI and new songs and more...
+
  IMPORTANT: THIS MAY NOT WORK ON CHROMIUM BASED BROWSERS!
 
 ˜”*°•.˜”*°• WELCOME •°*”˜.•°*”˜
 
 Thanks for using my shitty ass script.
 This script 'Parappaifies' the Google Search.
-Search something, and the script will judge the results, in 3 categories. "ALRIGHT", "BAD" and "AWFUL".
+Search something, and the script will judge the results, in 4 categories. "COOL", "ALRIGHT", "BAD" and "AWFUL".
 Totally open source, so you can edit it to your liking and fix the shitty code.
 
 
@@ -30,7 +32,8 @@ It'll then add 1 point or 2 points depending on how bad the word is.
 It then adds both numbers to make a final score.
 Depending on the final score, it's what the grading is.
 
-0-4: ALRIGHT
+<-10 : COOL
+-6 - 4: ALRIGHT
 4-14: BAD
 14>: AWFUL
 
@@ -50,6 +53,14 @@ Well, you opened the chart and you're like 'HOW TF DO I READ THIS?!'
 I made up a picture of how to read the chart.
 https://www.fryingpain.com/assets/parappa/chart.png
 
+UPDATE: STATUS 3 MEANS COOL (too lazy to change the image)
+
+˜”*°•.˜”*°• HOW TO GET COOL •°*”˜.•°*”˜
+
+Getting cool is similar to getting lower ranks.
+However, in order for the cool points to count, the status must be at alright.
+Fairly easy, just search up "cats" or "dogs". It's wholesome.
+
 Enjoy my script!
 Now i'm going to watch Murder Drones.
 
@@ -63,41 +74,60 @@ Now i'm going to watch Murder Drones.
     }
 
     const detectedOverlay = false; // Set to TRUE to see an overlay of the detected words. (You can also see the words on the console.)
+    const TakoyamaJumpscare = false; // Takoyama's Getting Better/Worse when changing ranks.
+
+    /* ˜”*°•.˜”*°• TRIGGER WORDS •°*”˜.•°*”˜
+
+    Remember. Trigger1 adds 1 point to the total score.
+    Trigger2 adds 2 points to the total score.
+
+    adorable REMOVES 1 point. This is for getting "COOL"
+
+    Feel free to edit those at your liking :3
+
+    Important: Words cannot have spaces. (sad.)
+    */
+    const Trigger1 = ['amor', 'genderswap', 'deviantart', 'furaffinity', 'affinity', 'wattpad', 'love', 'fat', 'goo', 'changed', 'suggestive', 'yaoi', 'yuri', 'lumity', 'rupphire'];
+    const Trigger2 = ['7w7', '7u7', 'r63', 'chubby', 'R18', 'fluff', 'rule', 'hentai', 'nsfw', 'cheeks', 'boozy' /*Fuck you enthuziastic. */ , 'fart', 'toot', 'feet', 'foot', 'vorarephilia', 'vore', 'inflation', 'scat', 'rule34', 'paheal', 'porn', 'dick', 'cum', 'smut', '+18', 'blushmallet', 'diarrhea', 'nightcrawler', 'dakimakura', 'bodypillow', '🔞', 'digestion', 'maid', 'bunny', 'feizao', 'notive', 'explicit', 'r34', 'diaper', 'r64'];
+    const adorable = ['cat', 'dog', 'kitty'];
+
+    /*
+    Song 1: Hair Scare - Parappa 2
+    Song 2: Instructor Mooselini's RAP - Parappa 1
+    Song 3: Chop Chop Master Onion's Rap (this one has lyrics) - Parappa 1
+    Song 4: Toasty Buns - Parappa 2
+	You can comment the code and make it force a song.
+    */
+    const song = 1 + random(4);
+
+    // Keep these the same, unless you wanna change it's behavior...
 
     let stat = "alright";
     let statn = 2;
     let statbef = statn;
 
     let totalScore = 0;
-    let ms = 0;
+    let ms = -1;
 
-    /* ˜”*°•.˜”*°• TRIGGER WORDS •°*”˜.•°*”˜
-
-    Remember. Trigger1 adds 1 point to the total score.
-    Trigger2 adds 2 points to the total score.
-    Feel free to edit those at your liking :3
-
-    Important: Words cannot have spaces. (sad.)
-    */
-    const Trigger1 = ['amor', 'genderswap', 'deviantart', 'furaffinity', 'affinity', 'wattpad', 'love', 'fat', 'goo', 'changed', 'suggestive', 'yaoi', 'yuri', 'lumity', 'rupphire'];
-    const Trigger2 = ['7w7','7u7','r63','chubby','R18','fluff', 'rule', 'hentai', 'nsfw', 'cheeks', 'boozy' /*Fuck you enthuziastic. */ , 'fart', 'toot', 'feet', 'foot', 'vorarephilia', 'vore', 'inflation', 'scat', 'rule34', 'paheal', 'porn', 'dick', 'cum', 'smut', '+18', 'blushmallet', 'diarrhea', 'nightcrawler', 'dakimakura', 'bodypillow', '🔞', 'digestion', 'maid', 'bunny', 'feizao', 'notive', 'explicit', 'r34', 'diaper', 'r64'];
+    let autoscroll = false;
 
     // The result's are ALRIGHT thing.
-    const overlay = document.createElement('div');
+    const overlay = document.createElement('img');
     overlay.style.position = 'fixed';
     overlay.style.bottom = '10px';
     overlay.style.left = '10px';
     overlay.style.backgroundColor = 'rgba(0, 0, 0, 0.7)';
-    overlay.style.color = 'white';
+    // overlay.style.color = 'white';
     overlay.style.padding = '15px';
     overlay.style.borderRadius = '5px';
     overlay.style.zIndex = '1000';
-    overlay.style.fontSize = '30px';
-    overlay.textContent = "The result's are ALRIGHT.";
+    overlay.style.width = '200px';
+    // overlay.style.fontSize = '30px';
+    // overlay.textContent = "The result's are ALRIGHT.";
 
     const topbad = document.createElement('div');
     topbad.style.position = 'fixed';
-    topbad.style.bottom = '10px';
+    topbad.style.top = '50px';
     topbad.style.right = '10px';
     topbad.style.backgroundColor = 'rgba(0, 0, 0, 0.7)';
     topbad.style.color = 'white';
@@ -108,7 +138,7 @@ Now i'm going to watch Murder Drones.
 
     const topawful = document.createElement('div');
     topawful.style.position = 'fixed';
-    topawful.style.bottom = '100px';
+    topawful.style.top = '100px';
     topawful.style.right = '10px';
     topawful.style.backgroundColor = 'rgba(150, 0, 0, 0.7)';
     topawful.style.color = 'white';
@@ -117,12 +147,20 @@ Now i'm going to watch Murder Drones.
     topawful.style.zIndex = '1000';
     topawful.style.fontSize = '25px';
 
-    /*
-    Song 1: Hair Scare - Parappa 2
-    Song 2: Instructor Mooselini's RAP - Parappa 1
-	You can comment the code and make it force a song.
-    */
-    const song = 1 + random(2);
+    const thumb = document.createElement('img');
+    thumb.style.position = 'fixed';
+    thumb.style.bottom = '250px';
+    thumb.style.left = '15px';
+    thumb.style.width = '70px';
+    thumb.src = ''
+
+    const gettbw = document.createElement('video');
+    gettbw.style.position = 'fixed';
+    gettbw.style.top = '0px';
+    gettbw.style.left = '0px';
+    gettbw.style.width = '100%';
+    gettbw.style.height = '100%';
+    gettbw.src = 'https://www.fryingpain.com/assets/parappa/gettingBetter.mp4'
 
     const increase = document.createElement('audio');
     increase.src = 'https://www.fryingpain.com/assets/parappa/getting_better.wav';
@@ -149,31 +187,43 @@ Now i'm going to watch Murder Drones.
     shutoff.src = 'https://www.fryingpain.com/assets/parappa/Shutoff.mp3';
     shutoff.volume = 1;
 
+    const cool = document.createElement('audio');
+    cool.src = 'https://www.fryingpain.com/assets/parappa/' + song + '/cool.mp3';
+    // alright.src = 'http://localhost:8000/alright.wav';
+    cool.loop = true;
+
+
     const alright = document.createElement('audio');
     alright.src = 'https://www.fryingpain.com/assets/parappa/' + song + '/alright.mp3';
+    // alright.src = 'http://localhost:8000/alright.wav';
     alright.loop = true;
 
     const bad = document.createElement('audio');
     bad.src = 'https://www.fryingpain.com/assets/parappa/' + song + '/bad.mp3';
+    // bad.src = 'http://localhost:8000/bad.wav';
     bad.loop = true;
 
     const awful = document.createElement('audio');
     awful.src = 'https://www.fryingpain.com/assets/parappa/' + song + '/awful.mp3';
+    // awful.src = 'http://localhost:8000/awful.wav';
     awful.loop = true;
 
-    const offset = 20;
+    const offset = 15;
 
+    cool.currentTime = offset;
     alright.currentTime = offset;
     bad.currentTime = offset;
     awful.currentTime = offset;
 
     const vars = {
+        cool: cool,
         alright: alright,
         bad: bad,
         awful: awful
     };
 
     document.body.appendChild(overlay);
+    document.body.appendChild(thumb);
 
     if (detectedOverlay) {
         document.body.appendChild(topawful);
@@ -203,13 +253,18 @@ Now i'm going to watch Murder Drones.
     }
 
     function mus(newValue) {
+        cool.play();
         alright.play();
         bad.play();
         awful.play();
+        cool.volume = 0;
         alright.volume = 0;
         bad.volume = 0;
         awful.volume = 0;
-        vars[stat].volume = 0.5;
+        // vars[stat].volume = 0.5;
+
+        // bad.currentTime = alright.currentTime;
+        // awful.currentTime = alright.currentTime;
 
         if (newValue === statn) {
             return;
@@ -221,10 +276,38 @@ Now i'm going to watch Murder Drones.
         if (statn > statbef) {
             increase.currentTime = 0;
             increase.play();
+            thumb.src = 'https://www.fryingpain.com/assets/parappa/thumbup.gif';
+            gettbw.src = 'https://www.fryingpain.com/assets/parappa/gettingBetter.mp4'
         } else {
             decrease.currentTime = 0;
             decrease.play();
+            thumb.src = 'https://www.fryingpain.com/assets/parappa/thumbdown.gif';
+            gettbw.src = 'https://www.fryingpain.com/assets/parappa/gettingWorse.mp4'
         }
+
+        if (TakoyamaJumpscare){
+            document.body.appendChild(gettbw);
+            gettbw.currentTime = 0;
+            gettbw.play();
+            if (autoscroll) {
+                clearInterval(intervalId);
+                clearInterval(updateChartIn);
+            }
+            setTimeout(function() {
+                document.body.removeChild(gettbw);
+                vars[stat].volume = 0.5;
+                if (autoscroll) {
+                    updateChartIn = setInterval(updateChart, 1000);
+                    intervalId = setInterval(scroll, 1);
+                }
+            }, 2300);
+        } else {
+            vars[stat].volume = 0.5;
+        }
+
+        setTimeout(function() {
+            thumb.src = '';
+        }, 750);
     }
 
     function scroll() {
@@ -238,8 +321,10 @@ Now i'm going to watch Murder Drones.
         const words = visibleText.match(/\b(\w+)\b/g) || [];
         let score1 = 0;
         let score2 = 0;
+        let score3 = 0;
         let detect1 = [];
         let detect2 = [];
+        let detect3 = [];
         words.forEach(word => {
             if (Trigger1.includes(word.toLowerCase())) {
                 score1++;
@@ -249,30 +334,42 @@ Now i'm going to watch Murder Drones.
                 score2 += 2;
                 detect2.push(word.toLowerCase());
             }
+            if (adorable.includes(word.toLowerCase())) {
+                score3 += 1;
+                detect3.push(word.toLowerCase());
+            }
         });
 
         totalScore = score1 + score2;
+
+        if (totalScore <= 4) {
+            totalScore = totalScore - score3;
+        }
         console.log(detect1);
         console.log(detect2);
         topbad.textContent = detect1;
         topawful.textContent = detect2;
         console.log(totalScore);
         if (totalScore > 14) {
-            ms = -1;
-            overlay.textContent = "The result's are AWFUL."
+            // overlay.textContent = "The result's are AWFUL."
             if (stat !== "awful") {
                 stat = "awful";
                 mus(0);
+            } else if (ms === 0) {
+                dbad.play();
+            } else if (ms === 1) {
+                dgood.play();
             }
+            ms = -1;
         } else if (totalScore > 10) {
             if (stat === "bad") {
-                overlay.textContent = "The result's are BAD | ▼"
+                // overlay.textContent = "The result's are BAD | ▼"
                 if (ms !== 1) {
                     ms = 1;
                     dbad.play();
                 }
             } else {
-                overlay.textContent = "The result's are AWFUL | ▲"
+                // overlay.textContent = "The result's are AWFUL | ▲"
                 if (ms !== 0) {
                     ms = 0;
                     dgood.play();
@@ -280,35 +377,68 @@ Now i'm going to watch Murder Drones.
             }
 
         } else if (totalScore > 6) {
-            ms = -1;
-            overlay.textContent = "The result's are BAD."
+            // overlay.textContent = "The result's are BAD."
             if (stat !== "bad") {
                 stat = "bad";
                 mus(1);
+            } else if (ms === 0) {
+                dbad.play();
+            } else if (ms === 1) {
+                dgood.play();
             }
+            ms = -1;
         } else if (totalScore > 4) {
             if (stat === "alright") {
-                overlay.textContent = "The result's are ALRIGHT | ▼"
+                // overlay.textContent = "The result's are ALRIGHT | ▼"
                 if (ms !== 1) {
                     ms = 1;
                     dbad.play();
                 }
             } else {
-                overlay.textContent = "The result's are BAD | ▲"
+                // overlay.textContent = "The result's are BAD | ▲"
+                if (ms !== 0) {
+                    ms = 0;
+                    dgood.play();
+                }
+            }
+        } else if (totalScore > -6) {
+            // overlay.textContent = "The result's are ALRIGHT."
+            if (stat !== "alright") {
+                stat = "alright";
+                mus(2);
+            } else if (ms === 0) {
+                dbad.play();
+            } else if (ms === 1) {
+                dgood.play();
+            }
+            ms = -1;
+        } else if (totalScore > -10) {
+            if (stat === "cool") {
+                if (ms !== 1) {
+                    ms = 1;
+                    dbad.play();
+                }
+            } else {
                 if (ms !== 0) {
                     ms = 0;
                     dgood.play();
                 }
             }
         } else {
-            ms = -1;
-            overlay.textContent = "The result's are ALRIGHT."
-            if (stat !== "alright") {
-                stat = "alright";
-                mus(2);
+            if (stat !== "cool") {
+                stat = "cool";
+                mus(3);
+            } else if (ms === 0) {
+                dbad.play();
+            } else if (ms === 1) {
+                dgood.play();
             }
+            ms = -1;
         }
-
+        let urlsito = 'https://www.fryingpain.com/assets/parappa/grade/' + stat + (ms + 1) + '.gif';
+        if (urlsito !== overlay.src) {
+            overlay.src = urlsito;
+        }
         // overlay.textContent = `Score: ${totalScore}`;
     }
 
@@ -362,10 +492,10 @@ Now i'm going to watch Murder Drones.
                             position: "left",
                             ticks: {
                                 min: 0,
-                                max: 2,
+                                max: 3,
                                 stepSize: 1,
                                 callback: function(value, index, values) {
-                                    return ['AWFUL', 'BAD', 'ALRIGHT'][value];
+                                    return ['AWFUL', 'BAD', 'ALRIGHT','COOL'][value]; // this doesn't work at all lmao
                                 }
                             }
                         },
@@ -401,7 +531,7 @@ Now i'm going to watch Murder Drones.
     let intervalId
     let updateChartIn
 
-    let autoscroll = false;
+    
 
     // Function to stop the interval when Q is pressed
     function stopFunction(event) {
@@ -421,8 +551,9 @@ Now i'm going to watch Murder Drones.
                 }, 700);
             };
             setTimeout(function() {
-                overlay.textContent = "SCRIPT TERMINATED. F5"
+                // overlay.textContent = "SCRIPT TERMINATED. F5"
                 scrolling.pause();
+                cool.pause();
                 alright.pause();
                 bad.pause();
                 awful.pause();
